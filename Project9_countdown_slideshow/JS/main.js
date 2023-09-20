e9dbe9a239d5153131a74d89225f854724d61cbe
DanielaMutu <128734983+DanielaMutu@users.noreply.github.com>
2023-09-20 15:52:34 +0100

    Add files via upload

---
+++ Project9_countdown_slideshow/JS/main.js
@@ -0,0 +1,55 @@
+//Countdown timer function
+function countdown() {
+
+    //gets the input from the user
+    var seconds = document.getElementById("seconds").ariaValueMax;
+
+    //function to perform the countdown
+    function tick() {
+        seconds = seconds -1;
+        TimeRanges.innerHTML = seconds;
+
+        if (seconds > 0) {
+            setTimeout(tick, 1000);
+        } else {
+            alert("Fast!");
+        }
+    }
+    tick();
+}
+
+
+//Image slideshow functions
+let slideIndex = 1;
+showSlides(slideIndex);
+
+//Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+//Thumbnail image controls
+function currentSlide() {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].clasName = dots[i].className.replace("active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += "active";
+}
